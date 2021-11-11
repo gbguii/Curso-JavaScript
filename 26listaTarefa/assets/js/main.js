dda9d695656075b01fa17aa9ded57bc8bec81004
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2021-11-11 10:34:23 -0300

    Finalizado JavaScript e CSS da ListaTarefa

--- 26listaTarefa/assets/js/main.js
+++ 26listaTarefa/assets/js/main.js
@@ -46,7 +46,29 @@ document.addEventListener("click", function(e){
     const el = e.target;
     if( el.classList.contains("apagar")){
         el.parentElement.remove();
+        salvarTarefa();
     }
 })
 
+function salvarTarefa(){
+    const liTarefas = tarefas.querySelectorAll("li");
+    const listaDeTarefas = [];
 
+    for (let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
+        listaDeTarefas.push(tarefaTexto)
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem("tarefas", tarefasJSON);
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem("tarefas");
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
