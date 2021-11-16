293f3c03687add789b46a7a4b317bd39d87bb5d6
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2021-11-15 22:14:34 -0300

    Aula maneiras de declaração função

---
+++ funcao-avancado/declaracao-funcao/index.js
@@ -0,0 +1,39 @@
+// Dentro de um objeto
+const obj = {
+    falar(){
+        console.log("Estou falando ...")
+    }
+}
+obj.falar()
+
+
+
+// Arrow function
+// const funcaoArrow = () => {
+//     console.log("Sou um arrow function")
+// }
+// funcaoArrow()
+
+
+
+
+
+// first-class objects (objetos de primeira classe)
+// function expression
+// const souUmDado = function(){
+//     console.log("Sou um dado.")
+// };
+// souUmDado()
+
+// function executaFuncao(funcao){
+//     funcao()
+// }
+// executaFuncao(souUmDado)
+
+
+
+
+// declaração de função (function hoisting)
+// function falaOi(){
+//     console.log("Oie")
+// }
