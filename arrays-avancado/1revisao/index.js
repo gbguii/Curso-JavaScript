c37c85c0c923100cf0fb985b7ca7b8fe1dd3eba7
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2021-11-25 20:54:45 -0300

    Começado modulo de array avançado

---
+++ arrays-avancado/1revisao/index.js
@@ -0,0 +1,16 @@
+// const nomes = ["Eduardo", "Maria", "Joana", "Walllace", "Rosana"]
+// nomes.pop(); - exclui ultimo
+//nomes.shift();;  exclui primeiro
+// nomes.push("Luiz");  adiciona no ultimo
+// nomes.unshift("Luiz");  adiciona no primeiro
+
+// const novo = nomes.slice(1,4);
+// console.log(novo);
+
+// const nome = "Luiz Guilherme Barbieri";
+// const nomes = nome.split(" ");
+// console.log(nomes);
+
+const nomes = [ 'Luiz', 'Guilherme', 'Barbieri' ];
+const nome = nomes.join(" ");
+console.log(nome)
